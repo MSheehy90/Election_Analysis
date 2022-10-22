f1ebba335821fdebc2d65392727785911754417d
Melinda <ponenalam@gmail.com>
2022-10-21 20:29:11 -0400

    Adding the election analysis.

---
+++ conditionals.py
@@ -0,0 +1,22 @@
+x = 5
+y = 10
+z = "10"
+
+if x == y:
+    print("X equal to Y")
+else:
+    print("X not equal to Y")
+
+if x > y:
+    print("X is great")
+else:
+    print("Y is great")
+
+if z == y:
+    print("Z and Y are equal")
+else:
+    print("Z and Y are not equal")
+if int(z) == y and x < y:
+    print ("Yay, it worked")
+else: 
+    print("No, forget it")
